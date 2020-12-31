a4db546a4b279faf5472f6da4cf5af9e12496fc2
merima98 <merimaceranic98@gmail.com>
2020-12-31 15:15:45 +0100

    Adding user data from GitHub - Modified User screen

--- src/features/User.js
+++ src/features/User.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 
 import NotFound from "./NotFound";
 import Header from "./Header";
+import { ArrowUpRight } from "react-feather";
 
 const Wrapper = styled.div`
   padding-top: 32px;
@@ -12,6 +13,80 @@ const Wrapper = styled.div`
   padding-right: 15rem;
 `;
 
+const SideBarContainer = styled.div`
+  color: #000;
+  font-size: 1rem;
+  font-family: Rubik;
+  width: 275px;
+`;
+
+const Name = styled.div`
+  background-color: #fff;
+`;
+
+const Heading = styled.h1`
+  font-size: 1rem;
+  color: #333;
+  font-family: Rubik;
+  display: inline;
+  margin: 0;
+`;
+
+const SideBar = styled.div`
+  background-color: #fff;
+  padding: 10px;
+  border: 2px solid #f7f7f7;
+  border-radius: 2px;
+  margin-bottom: 2px;
+`;
+
+const Item = styled.h3`
+  color: #888;
+  font-size: 14px;
+  font-family: Rubik;
+`;
+const Numbers = styled.p`
+  color: #555;
+  font-size: 15px;
+  margin-left: 95px;
+`;
+
+const List = styled.div`
+  padding: 0px 0px 0px 2.5px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-auto-rows: 30px;
+`;
+
+const ListLocation = styled.div`
+  padding: 0px 0px 0px 2.5px;
+  display: grid;
+`;
+
+const Loacation = styled.h4`
+  color: #888;
+  font-size: 12px;
+  font-family: Rubik;
+  margin: 0px 0px 1px 0px;
+`;
+
+const DateData = styled.p`
+  color: #555;
+  font-family: Rubik;
+  font-size: 14px;
+`;
+
+const LocationData = styled.p`
+  color: #5c75f6;
+  font-family: Rubik;
+  font-size: 14px;
+`;
+
+const Profile = styled.a`
+  text-decoration: none;
+  color: #333;
+  cursor: pointer;
+`;
 function User() {
   const [data, setData] = useState([]);
   const [isError, setIsError] = useState(false);
@@ -33,15 +108,54 @@ function User() {
       setIsError(true);
     }
   }, [setData]);
+
   return (
     <Wrapper>
       <Header />
       {isError ? (
         <NotFound />
       ) : (
-        <div>
-          <ul>{data.login}</ul>
-        </div>
+        <SideBarContainer>
+          <Name>
+            <Heading>{data.name}</Heading>
+            <Profile href={`https://github.com/${data.login}`}>
+              <ArrowUpRight />
+            </Profile>
+          </Name>
+          <SideBar>
+            <List>
+              <Item>Followers</Item>
+              <Numbers>1</Numbers>
+            </List>
+            <List>
+              <Item>Following</Item>
+              <Numbers>5</Numbers>
+            </List>
+            <List>
+              <Item>Stars Received</Item>
+              <Numbers>2</Numbers>
+            </List>
+            <List>
+              <Item>Forks by user</Item>
+              <Numbers>5</Numbers>
+            </List>
+          </SideBar>
+
+          <SideBar>
+            <ListLocation>
+              <Loacation>Joined</Loacation>
+              <DateData>{data.created_at}</DateData>
+            </ListLocation>
+            <ListLocation>
+              <Loacation>Loacation</Loacation>
+
+              <LocationData>{data.location}</LocationData>
+            </ListLocation>
+            <ListLocation>
+              <Item>Last update on {data.updated_at}</Item>
+            </ListLocation>
+          </SideBar>
+        </SideBarContainer>
       )}
     </Wrapper>
   );
